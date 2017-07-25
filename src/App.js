81c3174ff1f01dad41ed3b93ffebb446dcc91b79
Patryk  Huzarski <phuzarski@gmail.com>
2017-07-25 17:14:46 +0200

    Fetching the arrays, add filter

--- src/App.js
+++ src/App.js
@@ -2,34 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
-    this.state = {increasing: false}
+    this.state = { items: [] }
+}
+componentWillMount() {
+  fetch('https://swapi.co/api/people/?format=json')
+  .then(response => response.json())
+  .then (({results: items }) => this.setState({ items }))
+}
+filter(e) {
+  this.setState({ filter: e.target.value })
+}
+render(){
+  let items = this.state.items
+  if(this.state.filter) {
+    items = items.filter(item => item.name.toLowerCase().includes(this.state.filter.toLowerCase()))
   }
-  update(){
-    ReactDOM.render(
-      <App val={this.props.val + 1} />,
-      document.getElementById('root'))
-  }
-  componentWillReceiveProps(nextProps){
-    this.setState({increasing: nextProps.val > this.props.val})
-  }
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.val % 5 === 0;
-  }
-  render(){
-    console.log(this.state.increasing)
-    return (
-      <button onClick={this.update.bind(this)}>
-        {this.props.val}
-      </button>
-    )
-  }
-  componentDidUpdate(prevProps, prevState) {
-    console.log(`prevProps: ${prevProps.val}`)
-  }
+  return (
+    <div>
+      <input type="text" onChange={this.filter.bind(this)} />
+      {items.map(item => <Person key={item.name} person={item} />)}
+    </div>
+  )
+}
 }
 
+const Person = (props) => <h4>{props.person.name}</h4>
 
 App.defaultProps = { val: 0 }
 export default App;
