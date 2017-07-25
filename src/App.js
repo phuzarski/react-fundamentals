ca721af37e51d37b3ab998b394b8871e0ad068ac
Patryk  Huzarski <phuzarski@gmail.com>
2017-07-25 17:55:20 +0200

    React Children

--- src/App.js
+++ src/App.js
@@ -1,57 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-const HOC = (InnerComponent) => class extends React.Component {
-  constructor(){
-    super();
-    this.state = {count: 0}
-  }
-  update(){
-    this.setState({count: this.state.count + 1})
-  }
-  componentWillMount(){
-    console.log('will mount')
-  }
-  render(){
-    return (
-      <InnerComponent
-        {...this.props}
-        {...this.state}
-        update={this.update.bind(this)}
-      />
-    )
-  }
-}
-
 class App extends React.Component {
   render(){
     return (
       <div>
-        <Button>button</Button>
-        <hr/>
-        <LabelHOC>label</LabelHOC>
+        <Parent>
+          <div className="childA"></div>
+          {/* <div className="childB"></div> */}
+        </Parent>
       </div>
     )
   }
 }
 
-const Button = HOC((props) =>
-  <button onClick={props.update}>{props.children} - {props.count}</button>
-)
-
-class Label extends React.Component {
-  componentWillMount(){
-    console.log('label will mount')
-  }
-  render(){
-    return (
-      <label onMouseMove={this.props.update}>
-      {this.props.children} - {this.props.count}
-      </label>
-    )
+class Parent extends React.Component {
+  render() {
+    console.log(this.props.children)
+    let items = React.Children.map(this.props.children, child => child.props.className)
+    // let items2 = React.Children.toArray(this.props.children)
+    // console.log(items2)
+    let items3 = React.Children.only(this.props.children)
+    // console.log(items)
+    console.log(items3)
+    return null
   }
 }
-
-const LabelHOC = HOC(Label)
-
 export default App;
